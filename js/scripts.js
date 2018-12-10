a794b2f4e486bee3757bf473c5ea7515f4deb61c
Stella-Ndirangu <stellaw.ndirangu@gmail.com>
2018-12-10 14:20:20 +0300

    Add functions to take user input and get total score

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
+function scoreTotal(total, num){
+  return total + num;
+}
+
+
 $(document).ready(function() {
   $("#startQuiz").click(function(event) {
-    $("div.intro").hide();
+    $(".intro").hide();
     $("#quiz").show();
 
     event.preventDefault();
@@ -9,13 +14,23 @@ $(document).ready(function() {
 
 $(document).ready(function() {
   $("#quiz form").submit(function(event) {
+    var userAnswers = [];
+
+    for (i = 1; i <= 10; i++){
+      var inputAnswers = parseInt($("input:radio[name = q"+i+"]:checked").val());
+      userAnswers.push(inputAnswers);
+    }
+
+
+    alert(userAnswers);
+    var score = userAnswers.reduce(scoreTotal);
+    alert(score);
+
+
+
     $("#quiz").hide();
     $("#result").show();
 
     event.preventDefault();
   });
-});
-$(document).ready(function(){
-
-
 });
